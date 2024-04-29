5261afb4e2232ac0a44db14656cd500f5c23bcb8
Laura Wang Qiu <yiwang03@ucm.es>
2024-04-30 00:37:11 +0200

    Varios
    - Vídeo de la pantalla
    - Partículas de humo
    - Personas bailando
    - Zona de la entrada
    - Input Field condicionado
    - Arreglados los spotlights de los focos

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    private Animator animator;
+    private bool isDoorOpen = false;
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void ToggleDoor()
+    {
+        if (!isDoorOpen)
+        {
+            isDoorOpen = true;
+            animator.SetBool("isOpen", isDoorOpen);
+            if (audioSource != null)
+            {
+                audioSource.Play();
+            }
+        }
+    }
+}
--- Assets/Scripts/NameTransfer.cs
+++ Assets/Scripts/NameTransfer.cs
@@ -1,11 +1,14 @@
+using System.Text.RegularExpressions;
 using TMPro;
 using UnityEngine;
 
 public class NameTransfer : MonoBehaviour
 {
+    public GameObject door;
     public string playerName;
     public GameObject inputField;
     public GameObject textDisplay;
+    private TextMeshProUGUI textMeshPro;
 
     public void StoreName()
     {
@@ -15,9 +18,32 @@ public class NameTransfer : MonoBehaviour
             if (tmp != null)
             {
                 playerName = tmp.text;
-                if (textDisplay != null && textDisplay.GetComponent<TextMeshProUGUI>() != null)
+                textMeshPro = textDisplay.GetComponent<TextMeshProUGUI>();
+                if (textDisplay != null && textMeshPro != null)
                 {
-                    textDisplay.GetComponent<TextMeshProUGUI>().text = "¡Bienvenid@ " + playerName + "!";
+                    if (playerName.Length > 0)
+                    {
+                        if (playerName.Length >= 20)
+                        {
+                            if (Regex.IsMatch(playerName, @"\d"))
+                            {
+                                textMeshPro.text = "¡Bienvenido/a " + playerName + "!";
+                                door.GetComponent<DoorController>().ToggleDoor();
+                            }
+                            else
+                            {
+                                textMeshPro.text = "Tu nombre debe contener al menos un número.";
+                            }
+                        }
+                        else
+                        {
+                            textMeshPro.text = "Tu nombre debe contener al menos 20 caracteres.";
+                        }
+                    }
+                    else
+                    {
+                        textMeshPro.text = "Tu nombre no puede estar vacío.";
+                    }
                 }
             }
         }
